71cb5c15ea832f040e2ffcc298bea2a6f43e40a5
FRED F <FrederickFaour@github.com>
2019-07-13 10:09:37 -0400

    updated config and .env file for Heroku/JawsDB

--- server.js
+++ server.js
@@ -6,22 +6,16 @@ var app = express();
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body as JSON
+var PORT = process.env.PORT || 8080;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.text({ type: "text/html" }));
+app.use(express.static("app"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -30,7 +24,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("/controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 

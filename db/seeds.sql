d00a6f4cea087a0853558e026b75a5b0a9c1b25b
FRED F <FrederickFaour@github.com>
2019-07-13 09:31:14 -0400

    initial working program upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royal with Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Chicken Wing Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', true);

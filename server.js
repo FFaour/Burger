486c5a532cf40621a39047861c83e4a102aedfcf
FRED F <FrederickFaour@github.com>
2019-07-13 10:29:21 -0400

    Added variable configuration for local and Heroku connections

--- server.js
+++ server.js
@@ -3,19 +3,8 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
-// Parse application body as JSON
-var PORT = process.env.PORT || 8080;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.text({ type: "text/html" }));
-app.use(express.static("app"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
